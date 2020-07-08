c878ca94d268a9dd40363cfd286f3ecb9019fbed
RZFalcore <box.rz.box@gmail.com>
2020-07-08 14:09:34 +0300

    ..

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// const link = React.createElement(
+//   'a',
+//   {
+//     href: 'https://reactjs.org/',
+//     target: '_blank',
+//   },
+//   'React Website',
+// );
+
+// console.log(link);
+
+// const linkWithJSX = (
+//   <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
+//     React Website
+//   </a>
+// );
+
+// console.log(linkWithJSX);
+
+//-----------------------------------------------------------------------------------------
+
+// const image = React.createElement('img', {
+//   src:
+//     'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640',
+//   alt: 'Tacos With Lime',
+//   width: 640,
+// });
+// const title = React.createElement('h2', null, 'Tacos With Lime');
+// const price = React.createElement('p', null, 'Price: 10.99$');
+// const button = React.createElement('button', { type: 'button' }, 'Add to cart');
+
+// const product = React.createElement('div', null, image, title, price, button);
+
+// console.log(product);
+
+// ReactDOM.render(product, document.getElementById('root'));
+
+//-----------------------------------------------------------------------------------------
+
+// const productJSX = (
+//   <div>
+//     <img
+//       src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+//       alt="Tacos With Lime"
+//       width="640"
+//     />
+//     <h2>Tacos With Lime</h2>
+//     <p>Price: 10.99$</p>
+//     <button type="button">Add to cart</button>
+//   </div>
+// );
+
+// ReactDOM.render(productJSX, document.getElementById('root'));
+
+//-----------------------------------------------------------------------------------------
+// COMPONENT
+
+const Product = props => (
+  <div>
+    <img
+      src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+      alt="Tacos With Lime"
+      width="640"
+    />
+    <h2>Tacos With Lime</h2>
+    <p>Price: 10.99$</p>
+    <button type="button">Add to cart</button>
+  </div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Product />, document.getElementById('root'));
