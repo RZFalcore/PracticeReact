1da86938401de4f863e33745422fb4fbde1b1167
RZFalcore <box.rz.box@gmail.com>
2020-11-17 00:27:24 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div>Template</div>;
+import SignUpForm from "./components/SignUpForm/SignUpForm";
+
+class App extends Component {
+  handleFormSubmit = (credentials) => {
+    console.log(credentials);
+  };
+
+  render() {
+    return (
+      <div>
+        <SignUpForm onSignUp={this.handleFormSubmit} />
+      </div>
+    );
+  }
 }
 
 export default App;
