0b160a1a6f952c61c7fe9861732d5339ae7f6020
Eugene Roy <box.rz.box@gmail.com>
2021-01-28 17:02:37 +0200

    clear

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,38 +1,10 @@
 import React, { Component } from "react";
-import Menu from "../Menu/Menu";
-import faker from "faker";
-import Button from "../Button/Button";
-import List from "../List/List";
 class App extends Component {
-  state = {
-    items: [],
-  };
-
-  addItem = () => {
-    this.setState((state) => ({
-      items: [
-        ...state.items,
-        { id: faker.random.uuid(), text: faker.lorem.words(5) },
-      ],
-    }));
-  };
-
-  removeItem = (id) => {
-    this.setState((state) => ({
-      items: state.items.filter((item) => item.id !== id),
-    }));
-  };
+  state = {};
 
   render() {
-    const { items } = this.state;
     return (
-      <div>
-        <Menu />
-        <hr />
-        <Button label="Add item" onCLick={this.addItem} />
-        <hr />
-        <List items={items} onDelete={this.removeItem}   />
-      </div>
+      <div>App</div>
     );
   }
 }
