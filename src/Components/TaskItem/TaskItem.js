054c8e2a06b50947437f898cd0d2aa87f0ac796d
Eugene Roy <box.rz.box@gmail.com>
2021-02-03 00:45:31 +0200

    more styles, logic in TaskItem worked

--- src/Components/TaskItem/TaskItem.js
+++ src/Components/TaskItem/TaskItem.js
@@ -1,8 +1,21 @@
 import React from "react";
+import styles from "./TaskItem.module.css";
 
-const TaskItem = ({ id, text }) => (
-  <li key={id}>
-    <p>{text}</p>
+const TaskItem = ({ id, text, complited, onUpdate, onRemove }) => (
+  <li key={id} className={complited ? styles.listItemChecked : styles.listItem}>
+    <p className={styles.listItemText}>{text}</p>
+    <label className={styles.listItemLabel}>
+      <input
+        type="checkbox"
+        name="complited"
+        checked={complited}
+        onClick={onUpdate}
+      />
+      Complited
+      <button className={styles.listItemButton} onClick={onRemove}>
+        Delete
+      </button>
+    </label>
   </li>
 );
 
--- src/Components/TaskList/TaskList.js
+++ src/Components/TaskList/TaskList.js
@@ -2,9 +2,10 @@ import React from "react";
 import { connect } from "react-redux";
 import tasksActions from "../../redux/tasks/tasksActions";
 import TaskListItem from "../TaskItem/TaskItem";
+import styles from "./TaskList.module.css";
 
 const TaskList = ({ tasks, onRemoveTask, onUpdateTask }) => (
-  <ul className="TaskList">
+  <ul className={styles.taskList}>
     {tasks.map(({ id, text, complited }) => (
       <TaskListItem
         key={id}
