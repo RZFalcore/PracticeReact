df8954524d33322d472d672bf38e9ef47d07be2c
Eugene Roy <box.rz.box@gmail.com>
2021-02-03 01:08:40 +0200

    apll redux/toolkit on store and actionCreators

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,16 @@
-import { createStore, combineReducers } from "redux";
+// import {createStore, combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import tasksReducer from "./tasks/tasksReducer";
 
-const rootReducer = combineReducers({
-  tasks: tasksReducer,
-});
+// const rootReducer = combineReducers({
+//   tasks: tasksReducer,
+// });
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// const store = createStore(
+//   rootReducer,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+const store = configureStore({ reducer: { tasks: tasksReducer } });
 
 export default store;
