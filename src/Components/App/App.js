222dc209ad7d464034dad49bafdceb161d606a44
Eugene Roy <box.rz.box@gmail.com>
2021-02-07 23:52:41 +0200

    clean

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,7 @@
 import React from "react";
-import Layout from "../Layout/Layout";
-import TaskEditor from "../TaskEditor/TaskEditor";
-import TaskFilter from "../Filter/Filter";
-import TaskList from "../TaskList/TaskList";
 
 function App() {
-  return (
-    <div>
-      <Layout>
-        <TaskEditor />
-        <TaskFilter />
-        <TaskList />
-      </Layout>
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
