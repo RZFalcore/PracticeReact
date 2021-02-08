794de5f7520b2d6a242d969881b6648fc4cc365a
Eugene Roy <box.rz.box@gmail.com>
2021-02-08 12:59:36 +0200

    merg

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./Components/App/App";
 import "./index.css";
-
+console.log("Hi");
 ReactDOM.render(
   <Provider store={store}>
     <App />
