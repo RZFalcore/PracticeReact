1504af697774d3f5812b985d52f8df5136ace6c0
Eugene Roy <box.rz.box@gmail.com>
2021-02-17 21:39:37 +0200

    done

--- src/redux/tasks/tasksSelectors.js
+++ src/redux/tasks/tasksSelectors.js
@@ -6,24 +6,36 @@ const getLoading = (state) => state.tasks.loading;
 
 const getFilter = (state) => state.tasks.filter;
 
-const getVisibleTasks = createSelector([getTasks, getFilter], (tasks, filter) => {
-  return tasks.filter(({ text }) =>
-    text.toLowerCase().includes(filter.toLowerCase())
-  );
-});
-
+//---------------------------------------------------------------------------
+//! Memoised vers.
+const getVisibleTasks = createSelector(
+  [getTasks, getFilter],
+  (tasks, filter) => {
+    return tasks.filter(({ text }) =>
+      text.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
 // const getVisibleTasks = (state) => {
 //   const tasks = getTasks(state);
 //   const filter = getFilter(state).trim().toLowerCase();
 
 //   return tasks.filter(({ text }) => text.toLowerCase().includes(filter));
 // };
-
-const getTaskById = (state, id) => {
-  const tasks = getTasks(state);
-  return tasks.find((task) => task.id === id);
-};
-
+//---------------------------------------------------------------------------
+//! Memoised vers.
+const getTaskById = createSelector(
+  [getTasks, (state, id) => id],
+  (tasks, id) => {
+    return tasks.find((task) => task.id === id);
+  }
+  );
+  // const getTaskById = (state, id) => {
+    //   const tasks = getTasks(state);
+    //   return tasks.find((task) => task.id === id);
+    // };
+//---------------------------------------------------------------------------
+    
 const tasksSelectors = { getLoading, getFilter, getVisibleTasks, getTaskById };
 
 export default tasksSelectors;
